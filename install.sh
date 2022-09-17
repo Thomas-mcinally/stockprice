39ced0fa340a311772bbca9b193aa61812bdb08f
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-09-17 11:23:17 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "creating alias portfolio=${PWD}/portfolio.sh"
+alias portfolio="${PWD}/portfolio.sh"
+echo "creating alias info=${PWD}/stock_overview.sh"
+alias info="${PWD}/stock_overview.sh"
+
+echo "...done"
