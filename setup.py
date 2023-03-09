045d2275a2daccad898b3ce0fbeaa4313afc5e8b
Thomas-mcinally <95553476+Thomas-mcinally@users.noreply.github.com>
2023-03-09 01:14:48 +0000

    Add homepage (#16)
    * remove stockplot
    
    * add homepage in setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ setup(
     packages=find_packages(exclude=["tests"]),
     email="thomasmcinally@hotmail.com",
     author="Thomas Mcinally",
+    url="https://github.com/Thomas-mcinally/stockplot",
     long_description_content_type="text/markdown",
     install_requires=["yfinance", "pandas"],
-    version="1.0.0",
+    version="1.0.1",
     entry_points={"console_scripts": ["stockprice = stockprice.main:main"]},
 )
