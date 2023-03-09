099f8e6066684d25f868e8f415db888791750e2f
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-09 14:45:36 +0000

    add correct hompage url

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     packages=find_packages(exclude=["tests"]),
     email="thomasmcinally@hotmail.com",
     author="Thomas Mcinally",
-    url="https://github.com/Thomas-mcinally/stockplot",
+    url="https://github.com/Thomas-mcinally/stockprice",
     long_description_content_type="text/markdown",
     install_requires=["yfinance", "pandas"],
-    version="1.0.1",
+    version="1.0.2",
     entry_points={"console_scripts": ["stockprice = stockprice.main:main"]},
 )
