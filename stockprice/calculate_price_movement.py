3bffcfeb0ed1228e1f1e5b9d4a8bb1fd8b25f34f
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-13 22:07:22 +0000

    use yfinance api directly - iterate through all timestamps

--- stockprice/calculate_price_movement.py
+++ stockprice/calculate_price_movement.py
@@ -1,35 +1,25 @@
 import datetime
-import pandas as pd
-import yfinance
-from typing import Tuple
+from typing import List, Tuple
+import requests
 
 
-def calculate_percentage_price_change(current_price, start_price):
-    return (current_price - start_price) / start_price * 100
+# def calculate_percentage_price_change(current_price, start_price):
+#     return (current_price - start_price) / start_price * 100
 
-
-def calculate_percentage_price_change_1_day(data_1day, current_price):
-    price_at_start_of_day = data_1day.iloc[0, 0]
-    return calculate_percentage_price_change(current_price, price_at_start_of_day)
-
-
-def calculate_percentage_price_change_over_n_days(
-    n: int, current_price: float, data_90day: pd.DataFrame
-) -> float:
+def new_calculate_percentage_price_change_over_n_days(n, timestamps:List[int], closing_prices: List[int]):
     datetime_n_days_ago = datetime.datetime.now() - datetime.timedelta(days=n)
-    processing = True
-    while processing:
-        try:
-            date_n_days_ago = datetime_n_days_ago.strftime("%Y-%m-%d")
-            price_n_days_ago = data_90day.loc[date_n_days_ago, "Close"]
-            change_nday = calculate_percentage_price_change(
-                current_price, price_n_days_ago
-            )
-            processing = False
-        except KeyError:
-            # no data because this day was not a trading day
-            datetime_n_days_ago = datetime_n_days_ago - datetime.timedelta(days=1)
-    return change_nday
+    date_n_days_ago = datetime_n_days_ago.date()
+
+    index_of_price = None
+    for index in range(len(timestamps)):
+        past_date = datetime.datetime.fromtimestamp(timestamps[index]).date()
+        if past_date > date_n_days_ago:
+            break
+        index_of_price = index
+
+    price_n_days_ago = closing_prices[index_of_price]
+    current_price = closing_prices[-1]
+    return 100 * (current_price - price_n_days_ago) / price_n_days_ago 
 
 
 def calculate_price_movement(ticker: str) -> Tuple[float, float, float]:
@@ -43,27 +33,17 @@ def calculate_price_movement(ticker: str) -> Tuple[float, float, float]:
         percentage_change_7day (float): Price change since last trading day >=7days ago
         percentage_change_30day (float): Price change since last trading day >=30days ago
     """
-    data_90day: pd.DataFrame = yfinance.download(
-        ticker, period="90d", interval="1d", auto_adjust=True, progress=False
+    response = requests.get(
+        f"https://query2.finance.yahoo.com/v8/finance/chart/{ticker}?interval=1d&range=30d",
+        headers={"User-Agent": "Mozilla/5.0"},
     )
-    data_1day: pd.DataFrame = yfinance.download(
-        ticker, period="1d", interval="1m", auto_adjust=True, progress=False
-    )
+    closing_prices = response.json()["chart"]["result"][0]["indicators"]["quote"][0]["close"]
+    timestamps = response.json()["chart"]["result"][0]["timestamp"]
+    current_price = closing_prices[-1]
 
-    current_price = data_1day.iloc[-1, 3]
-    percentage_change_30day = calculate_percentage_price_change_over_n_days(
-        30, current_price, data_90day
-    )
-    percentage_change_7day = calculate_percentage_price_change_over_n_days(
-        7, current_price, data_90day
-    )
-    percentage_change_1day = calculate_percentage_price_change_1_day(
-        data_1day, current_price
-    )
 
-    return (
-        current_price,
-        percentage_change_1day,
-        percentage_change_7day,
-        percentage_change_30day,
-    )
+    percentage_change_30day = new_calculate_percentage_price_change_over_n_days(30, timestamps,closing_prices)
+    percentage_change_7day = new_calculate_percentage_price_change_over_n_days(7, timestamps,closing_prices)
+    percentage_change_1day = new_calculate_percentage_price_change_over_n_days(1, timestamps,closing_prices)
+
+    return (current_price,percentage_change_1day,percentage_change_7day,percentage_change_30day)
--- tests/test_stockprice.py
+++ tests/test_stockprice.py
@@ -1,58 +1,47 @@
 import freezegun
-from tests.conftest import (
-    example_yahoo_api_response_90day_aapl_2023_03_01,
-    example_yahoo_api_response_1day_aapl_2023_03_01,
-    example_yahoo_api_response_90day_tsla_2023_03_01,
-    example_yahoo_api_response_1day_tsla_2023_03_01,
-)
+from tests.conftest import example_yahoo_api_response_30day_tsla_2023_03_13
 from stockprice.main import main
 
-
-@freezegun.freeze_time("2023-03-01")
+@freezegun.freeze_time("2023-03-13")
 def test_stockprice_with_one_ticker(
-    capsys,
-    mock_GET_yahoo_v8_finance_chart_api_1day_range,
-    mock_GET_yahoo_v8_finance_chart_api_90day_range,
+    capsys, mock_GET_yahoo_v8_finance_chart_api_30day_range
 ):
-    mock_GET_yahoo_v8_finance_chart_api_90day_range(
-        "AAPL", example_yahoo_api_response_90day_aapl_2023_03_01
-    )
-    mock_GET_yahoo_v8_finance_chart_api_1day_range(
-        "AAPL", example_yahoo_api_response_1day_aapl_2023_03_01
+    mock_GET_yahoo_v8_finance_chart_api_30day_range(
+        "TSLA", example_yahoo_api_response_30day_tsla_2023_03_13
     )
 
-    main(["stockprice", "aapl"])
+    main(["stockprice", "tsla"])
 
     stdout = capsys.readouterr().out
     assert (
         stdout
-        == "AAPL -- Current price: 145.09 -- Daily change: -1.23%, 7-day change: -2.57%, 30-day change: 1.62%\n"
+        == "TSLA -- Current price: 174.95 -- Daily change: 0.87%, 7-day change: -9.73%, 30-day change: -11.14%\n"
     )
 
 
-@freezegun.freeze_time("2023-03-01")
-def test_stockprice_with_two_tickers(
-    capsys,
-    mock_GET_yahoo_v8_finance_chart_api_1day_range,
-    mock_GET_yahoo_v8_finance_chart_api_90day_range,
-):
-    mock_GET_yahoo_v8_finance_chart_api_90day_range(
-        "AAPL", example_yahoo_api_response_90day_aapl_2023_03_01
-    )
-    mock_GET_yahoo_v8_finance_chart_api_1day_range(
-        "AAPL", example_yahoo_api_response_1day_aapl_2023_03_01
-    )
-    mock_GET_yahoo_v8_finance_chart_api_90day_range(
-        "TSLA", example_yahoo_api_response_90day_tsla_2023_03_01
-    )
-    mock_GET_yahoo_v8_finance_chart_api_1day_range(
-        "TSLA", example_yahoo_api_response_1day_tsla_2023_03_01
-    )
+# @freezegun.freeze_time("2023-03-01")
+# def test_stockprice_with_two_tickers(
+#     capsys,
+#     mock_GET_yahoo_v8_finance_chart_api_1day_range,
+#     mock_GET_yahoo_v8_finance_chart_api_90day_range,
+# ):
+#     mock_GET_yahoo_v8_finance_chart_api_90day_range(
+#         "AAPL", example_yahoo_api_response_90day_aapl_2023_03_01
+#     )
+#     mock_GET_yahoo_v8_finance_chart_api_1day_range(
+#         "AAPL", example_yahoo_api_response_1day_aapl_2023_03_01
+#     )
+#     mock_GET_yahoo_v8_finance_chart_api_90day_range(
+#         "TSLA", example_yahoo_api_response_90day_tsla_2023_03_01
+#     )
+#     mock_GET_yahoo_v8_finance_chart_api_1day_range(
+#         "TSLA", example_yahoo_api_response_1day_tsla_2023_03_01
+#     )
 
-    main(["stockprice", "aapl,tsla"])
+#     main(["stockprice", "aapl,tsla"])
 
-    stdout = capsys.readouterr().out
-    assert (
-        stdout
-        == "AAPL -- Current price: 145.09 -- Daily change: -1.23%, 7-day change: -2.57%, 30-day change: 1.62%\nTSLA -- Current price: 202.75 -- Daily change: -1.62%, 7-day change: 0.94%, 30-day change: 21.65%\n"
-    )
+#     stdout = capsys.readouterr().out
+#     assert (
+#         stdout
+#         == "AAPL -- Current price: 145.09 -- Daily change: -1.23%, 7-day change: -2.57%, 30-day change: 1.62%\nTSLA -- Current price: 202.75 -- Daily change: -1.62%, 7-day change: 0.94%, 30-day change: 21.65%\n"
+#     )
