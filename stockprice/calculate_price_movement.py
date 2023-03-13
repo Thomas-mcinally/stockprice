100d763cec4fc391314f62457d45238747e7b417
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-13 23:43:13 +0000

    remove comments

--- stockprice/calculate_price_movement.py
+++ stockprice/calculate_price_movement.py
@@ -61,9 +61,3 @@ def calculate_price_movement(ticker: str) -> Tuple[float, float, float]:
         percentage_change_7day,
         percentage_change_30day,
     )
-
-
-# todo:
-# Does this work for different time zones?
-# Need to ensure time zone used by datetime is the same as is used by timestamps from request
-# Can we optimize search through timestamps array? Maybe with binary search?
