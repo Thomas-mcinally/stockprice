2d90721821c595fe72269398cd5e5ec48e6b043f
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2023-03-16 19:31:09 +0000

    update dependencies to not clash with stockplot

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
         "charset-normalizer==3.1.0; python_full_version >= '3.7.0'",
         "idna==3.4; python_version >= '3.5'",
         "requests==2.28.2",
-        "urllib3==1.26.15; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5'",
+        "urllib3==1.26.14; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4, 3.5'",
     ],
     version="2.0.2",
     entry_points={"console_scripts": ["stockprice = stockprice.main:main"]},
